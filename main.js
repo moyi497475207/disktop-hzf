ac70d13e2aa6910b0231f8717a45bc4b7cb84992
huzifan <497475207@qq.com>
2017-06-11 18:39:04 +0800

    first commin

---
+++ main.js
@@ -0,0 +1,300 @@
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+
+const Menu = electron.Menu;
+
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+let template = [{
+    label: '编辑',
+    submenu: [{
+        label: '撤销',
+        accelerator: 'CmdOrCtrl+Z',
+        role: 'undo'
+    }, {
+        label: '重做',
+        accelerator: 'Shift+CmdOrCtrl+Z',
+        role: 'redo'
+    }, {
+        type: 'separator'
+    }, {
+        label: '剪切',
+        accelerator: 'CmdOrCtrl+X',
+        role: 'cut'
+    }, {
+        label: '复制',
+        accelerator: 'CmdOrCtrl+C',
+        role: 'copy'
+    }, {
+        label: '粘贴',
+        accelerator: 'CmdOrCtrl+V',
+        role: 'paste'
+    }, {
+        label: '全选',
+        accelerator: 'CmdOrCtrl+A',
+        role: 'selectall'
+    }]
+}, {
+    label: '查看',
+    submenu: [{
+        label: '重载',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                // 重载之后, 刷新并关闭所有的次要窗体
+                if (focusedWindow.id === 1) {
+                    BrowserWindow.getAllWindows().forEach(function (win) {
+                        if (win.id > 1) {
+                            win.close()
+                        }
+                    })
+                }
+                focusedWindow.reload()
+            }
+        }
+    }, {
+        label: '切换全屏',
+        accelerator: (function () {
+            if (process.platform === 'darwin') {
+                return 'Ctrl+Command+F'
+            } else {
+                return 'F11'
+            }
+        })(),
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+            }
+        }
+    }, {
+        label: '切换开发者工具',
+        accelerator: (function () {
+            if (process.platform === 'darwin') {
+                return 'Alt+Command+I'
+            } else {
+                return 'F12'
+            }
+        })(),
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                focusedWindow.toggleDevTools()
+            }
+        }
+    }, {
+        type: 'separator'
+    }, {
+        label: '应用程序菜单演示',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                const options = {
+                    type: 'info',
+                    title: '应用程序菜单演示',
+                    buttons: ['好的'],
+                    message: '此演示用于 "菜单" 部分, 展示如何在应用程序菜单中创建可点击的菜单项.'
+                };
+                electron.dialog.showMessageBox(focusedWindow, options, function () {
+                })
+            }
+        }
+    }]
+}, {
+    label: '窗口',
+    role: 'window',
+    submenu: [{
+        label: '最小化',
+        accelerator: 'CmdOrCtrl+M',
+        role: 'minimize'
+    }, {
+        label: '关闭',
+        accelerator: 'CmdOrCtrl+W',
+        role: 'close'
+    }, {
+        type: 'separator'
+    }, {
+        label: '重新打开窗口',
+        accelerator: 'CmdOrCtrl+Shift+T',
+        enabled: false,
+        key: 'reopenMenuItem',
+        click: function () {
+            app.emit('activate')
+        }
+    }]
+}, {
+    label: '帮助',
+    role: 'help',
+    submenu: [{
+        label: '学习更多',
+        click: function () {
+            electron.shell.openExternal('http://electron.atom.io')
+        }
+    }]
+}];
+
+function addUpdateMenuItems(items, position) {
+    if (process.mas) return;
+
+    const version = electron.app.getVersion();
+    let updateItems = [{
+        label: 'Version ${version}',
+        enabled: false
+    }, {
+        label: '正在检查更新',
+        enabled: false,
+        key: 'checkingForUpdate'
+    }, {
+        label: '检查更新',
+        visible: false,
+        key: 'checkForUpdate',
+        click: function () {
+            require('electron').autoUpdater.checkForUpdates()
+        }
+    }, {
+        label: '重启并安装更新',
+        enabled: true,
+        visible: false,
+        key: 'restartToUpdate',
+        click: function () {
+            require('electron').autoUpdater.quitAndInstall()
+        }
+    }];
+
+    items.splice.apply(items, [position, 0].concat(updateItems))
+}
+
+function findReopenMenuItem() {
+    const menu = Menu.getApplicationMenu()
+    if (!menu) return
+
+    let reopenMenuItem
+    menu.items.forEach(function (item) {
+        if (item.submenu) {
+            item.submenu.items.forEach(function (item) {
+                if (item.key === 'reopenMenuItem') {
+                    reopenMenuItem = item
+                }
+            })
+        }
+    });
+    return reopenMenuItem
+}
+
+if (process.platform === 'darwin') {
+    const name = electron.app.getName()
+    template.unshift({
+        label: name,
+        submenu: [{
+            label: '关于 ${name}',
+            role: 'about'
+        }, {
+            type: 'separator'
+        }, {
+            label: '服务',
+            role: 'services',
+            submenu: []
+        }, {
+            type: 'separator'
+        }, {
+            label: '隐藏 ${name}',
+            accelerator: 'Command+H',
+            role: 'hide'
+        }, {
+            label: '隐藏其它',
+            accelerator: 'Command+Alt+H',
+            role: 'hideothers'
+        }, {
+            label: '显示全部',
+            role: 'unhide'
+        }, {
+            type: 'separator'
+        }, {
+            label: '退出',
+            accelerator: 'Command+Q',
+            click: function () {
+                app.quit()
+            }
+        }]
+    });
+
+    // 窗口菜单
+    template[3].submenu.push({
+        type: 'separator'
+    }, {
+        label: '前置所有',
+        role: 'front'
+    });
+
+    addUpdateMenuItems(template[0].submenu, 1)
+}
+
+if (process.platform === 'win32') {
+    const helpMenu = template[template.length - 1].submenu;
+    addUpdateMenuItems(helpMenu, 0)
+}
+
+
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 1920, height: 1200});
+
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
+    })
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+app.on('browser-window-created', function () {
+    let reopenMenuItem = findReopenMenuItem();
+    if (reopenMenuItem) reopenMenuItem.enabled = false
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+
+    let reopenMenuItem = findReopenMenuItem();
+    if (reopenMenuItem) reopenMenuItem.enabled = true
+});
+
+app.on('activate', function () {
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
